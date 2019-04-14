0a173f0b0d224117b523bc38e012982d5be1c9cd
AlexeyPakhomov <rivermc@alarstudios.com>
2019-04-14 13:44:01 +0200

    b2b page, callback, typography

--- assets/template/js/callback.js
+++ assets/template/js/callback.js
@@ -13,21 +13,21 @@ $(document).ready(function(){
       cache:false,
       beforeSend: function() {
         $('#' + idForm + ' button').prop('disabled', true);
-        callback('true');
+        callback('true', idForm);
       },
       success:function(data) {
-        callback(data);
+        callback(data, idForm);
         $('#' + idForm + ' button').prop('disabled', false);
       }
     });
   }
 
 
-  function callback(check) {
+  function callback(check, idForm) {
 
     if (check == 'true') {
-      $('#callback__form').fadeOut(500).delay(8000).queue(function(next) {
-        $('#callback__form').fadeIn(500);
+      $('#' + idForm).fadeOut(500).delay(8000).queue(function(next) {
+        $('#' + idForm).fadeIn(500);
         $('.form__callback_error').empty();
         next();
       });
@@ -57,19 +57,27 @@ $(document).ready(function(){
     var FormId = $(this).parents('form').attr('id');
     var inputs = $(this).parents('form').find('input');
     var test = true;
+    var send_check = [];
     
     $(inputs).each(function(index, item) {
       var inputName = $(item).attr('name');
       if ( inputName == 'email') {
         var valueInputEmail = $(item).val();
         test = isEmail(valueInputEmail);
+        send_check.push(test);
       }
       else if (inputName == 'phone') {
         var valueInputPhone = $(item).val();
         test = isPhone(valueInputPhone);
+        send_check.push(test);
       }
       if (test == false) {
         $('.callback__form' + ' .callback__form_input_' + $(item).attr('name')).addClass('shake').delay(800).queue(function(next){ $('.callback__form .callback__form_input_' + $(item).attr('name')).removeClass('shake');  next(); });
+      }
+    });
+    send_check.forEach(function(item){
+      if (item == false) {
+        test = false;
       }
     });
     if (test == true) {
@@ -77,23 +85,22 @@ $(document).ready(function(){
     }
   });
 
-  $(document).on('focus', '.callback__form_input', function(){
-    var callback_group = $(this).parents('.form__callback_form-group');
+  $('.callback__form_input').focus(function(){
+    var callback_group = $(this).parent();
+    callback_group.addClass('focused');
+  });
+
+  $('.form__group_label').click(function(){
+    var callback_group = $(this).parent();
     callback_group.addClass('focused');
     callback_group.find('input').focus();
   });
 
-  $(document).on('click', '.form__group_label', function(){
-    var callback_group = $(this).parents('.form__callback_form-group');
-    callback_group.addClass('focused');
-    callback_group.find('input').focus();
-  });
-
-  $(document).on('blur', '.callback__form_input', function(){
+  $('.callback__form_input').blur(function(){
     var inputValue = $(this).val();
-    if ( (inputValue == "")) {
+    if (inputValue == "") {
       $(this).removeClass('filled');
-      $(this).parents('.form__callback_form-group').removeClass('focused');
+      $(this).parent().removeClass('focused');
     } else {
       $(this).addClass('filled');
     }
