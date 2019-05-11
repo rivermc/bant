44d7ef7d46716d925d7abaa67e8cf6ae968fa78c
AlexeyPakhomov <rivermc@alarstudios.com>
2019-05-11 15:46:40 +0200

    special link in admin page, fix search form

--- assets/template/js/search.js
+++ assets/template/js/search.js
@@ -1,10 +1,10 @@
 
 
 $(document).ready(function(){
-	$('.header__search p, .header__search svg').click(function(){
-		$('.search_form_cont').addClass('active');
+	$('.search_input').focus(function(){
+		$(this).addClass('active');
 	});
-	$('.search__form_close').click(function() {
-		$('.search_form_cont').removeClass('active');
+	$('.search_input').blur(function(){
+		$(this).removeClass('active');
 	});
 });
