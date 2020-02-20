c4854e7aeff41d5b530ff90a810ea7a733126a63
AlexeyPakhomov <web@2f-vsk.ru>
2020-02-20 18:39:28 +0100

    Add sortby for add to order products

--- assets/template/js/add_to_order.js
+++ assets/template/js/add_to_order.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
   getModule(
     'Snippet',
     'msProducts',
-    'parents => 447, tpl => CatalogItem, limit => 12, where => {"template": "8"}',
+    'parents => 447, tpl => CatalogItem, limit => 12, where => {"template": "8"}, sortby => {"Data.popular":"DESC"}',
     function (data) {
       $('.js_slick_add_to_order').slick('unslick');
       $('.js_add_to_order_target').html(data);
